26d09fa122f4894281e60166b1acba7fe9e2b015
liukun <liu_kun@msn.com>
2017-06-14 23:33:10 +0800

    设置url采用api开头的页面的返回类型为json

--- web/src/main/java/web/handler/impl/WebAuthHandlerImpl.java
+++ web/src/main/java/web/handler/impl/WebAuthHandlerImpl.java
@@ -15,15 +15,16 @@ import java.lang.reflect.Method;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
 /**
  * Created by liulaoye on 17-6-12.
  * 自定义的权限检测模块
  */
-public class WebAuthHandlerImpl implements WebAuthHandler{
-    private static final Logger logger = LoggerFactory.getLogger( WebAuthHandlerImpl.class.getName() );
+public class WebAuthHandlerImpl implements WebAuthHandler {
+    private static final Logger logger = LoggerFactory.getLogger(WebAuthHandlerImpl.class.getName());
     private static final String PACKAGE_BASE = "web.handler.impl";
 
-    private static class PermisstionAndRoleSet{
+    private static class PermisstionAndRoleSet {
         private Set<String> permissions = new HashSet<>();
         private Set<String> roles = new HashSet<>();
         private Set<String> all = new HashSet<>();
@@ -35,27 +36,27 @@ public class WebAuthHandlerImpl implements WebAuthHandler{
 //            all.addAll( roles );
 //        }
 
-        void addRoles( Set<String> rolesSet ){
-            roles.addAll( rolesSet );
-            all.addAll( roles );
+        void addRoles(Set<String> rolesSet) {
+            roles.addAll(rolesSet);
+            all.addAll(roles);
         }
 
-        void addPermissions( Set<String> permissionsSet ){
-            permissions.addAll( permissionsSet );
-            all.addAll( permissionsSet );
+        void addPermissions(Set<String> permissionsSet) {
+            permissions.addAll(permissionsSet);
+            all.addAll(permissionsSet);
         }
 
         @Override
-        public String toString(){
+        public String toString() {
             return "PermisstionAndRoleSet{" +
                     "permissions=" + permissions +
                     ", roles=" + roles +
                     '}';
         }
 
-        boolean isExist( Set<String> set ){
-            for( String s : set ) {
-                if( all.contains( s ) ) {
+        boolean isExist(Set<String> set) {
+            for (String s : set) {
+                if (all.contains(s)) {
                     return true;
                 }
             }
@@ -68,39 +69,39 @@ public class WebAuthHandlerImpl implements WebAuthHandler{
      */
     private static final Map<String, PermisstionAndRoleSet> authMap = new HashMap<>();
 
-    static{
-        List<Class<?>> list = PackageUtil.getClasses( PACKAGE_BASE );
+    static {
+        List<Class<?>> list = PackageUtil.getClasses(PACKAGE_BASE);
 
-        for( Class<?> clazz : list ) {
-            parseClass( clazz );
+        for (Class<?> clazz : list) {
+            parseClass(clazz);
         }
-        logger.info( authMap.toString() );
+        logger.info(authMap.toString());
 
     }
 
     @SuppressWarnings("unused")
-    public WebAuthHandlerImpl( AuthProvider authProvider ){
+    public WebAuthHandlerImpl(AuthProvider authProvider) {
 
     }
 
-    private static void parseClass( Class<?> clazz ){
+    private static void parseClass(Class<?> clazz) {
         final Method[] methods = clazz.getDeclaredMethods();
 
-        for( Method method : methods ) {
+        for (Method method : methods) {
 
-            if( method.isAnnotationPresent( RequirePermissions.class ) || method.isAnnotationPresent( RequireRoles.class ) ) {
-                final String clazzName = getClassName( clazz );
+            if (method.isAnnotationPresent(RequirePermissions.class) || method.isAnnotationPresent(RequireRoles.class)) {
+                final String clazzName = getClassName(clazz);
                 PermisstionAndRoleSet roleAndPermisstionSet = new PermisstionAndRoleSet();
 
-                if( method.isAnnotationPresent( RequirePermissions.class ) ) {
-                    roleAndPermisstionSet.addPermissions( transSetFromStr( method.getDeclaredAnnotation( RequirePermissions.class ).value() ) );
+                if (method.isAnnotationPresent(RequirePermissions.class)) {
+                    roleAndPermisstionSet.addPermissions(transSetFromStr(method.getDeclaredAnnotation(RequirePermissions.class).value()));
                 }
-                if( method.isAnnotationPresent( RequireRoles.class ) ) {
-                    roleAndPermisstionSet.addRoles( transSetFromStr( method.getDeclaredAnnotation( RequireRoles.class ).value() ) );
+                if (method.isAnnotationPresent(RequireRoles.class)) {
+                    roleAndPermisstionSet.addRoles(transSetFromStr(method.getDeclaredAnnotation(RequireRoles.class).value()));
 
                 }
                 String key = clazzName + "/" + method.getName();
-                authMap.put( key, roleAndPermisstionSet );
+                authMap.put(key, roleAndPermisstionSet);
             }
         }
 
@@ -112,8 +113,8 @@ public class WebAuthHandlerImpl implements WebAuthHandler{
      * @param str 要分割的字符串
      * @return set
      */
-    private static Set<String> transSetFromStr( String str ){
-        return Arrays.stream( str.split( "," ) ).collect( Collectors.toSet() );
+    private static Set<String> transSetFromStr(String str) {
+        return Arrays.stream(str.split(",")).collect(Collectors.toSet());
     }
 
     /**
@@ -125,39 +126,51 @@ public class WebAuthHandlerImpl implements WebAuthHandler{
      * @param clazz class
      * @return class name
      */
-    private static String getClassName( Class<?> clazz ){
+    private static String getClassName(Class<?> clazz) {
         String canonicalName = clazz.getCanonicalName();
-        canonicalName = canonicalName.substring( PACKAGE_BASE.length() + 1 ).replace( "Handler", "" );
+        canonicalName = canonicalName.substring(PACKAGE_BASE.length() + 1).replace("Handler", "");
         return canonicalName.toLowerCase();
     }
 
     @Override
-    public void handle( RoutingContext ctx ){
+    public void handle(RoutingContext ctx) {
+        ctx.addHeadersEndHandler(v -> {
+            //
+//            Set<Cookie> cookies = ctx.cookies();
+//            for (Cookie cookie: cookies) {
+//                if (cookie.isChanged()) {
+//                    ctx.response().headers().add(SET_COOKIE, cookie.encode());
+//                }
+//            }
+            ctx.response().putHeader("Content-Type", "application/json");
+        });
+
         final CustomWebUser user = (CustomWebUser) ctx.user();
 
-        if( user == null ) {
-            ctx.fail( 403 );
-
+        if (user == null) {
+            ctx.fail(403);
+            return;
 //           ctx.response().end("Not loggin");
-        } else {
-            String path = ctx.request().path().substring( AuthWebVerticle.API_PREFIX.length() );
-            PermisstionAndRoleSet permissionOrRole = authMap.get( path );
-            if( permissionOrRole == null ) {
-                ctx.fail( 403 );
-                return;
-            }
-
-            if( doIsPermitted( user, permissionOrRole ) )
-                ctx.next();
-            else
-                ctx.fail( 403 );
         }
+        String path = ctx.request().path().substring(AuthWebVerticle.API_PREFIX.length());
+        PermisstionAndRoleSet permissionOrRole = authMap.get(path);
+        if (permissionOrRole == null) {
+            ctx.fail(403);
+            return;
+        }
+
+        if (doIsPermitted(user, permissionOrRole))
+            ctx.next();
+        else
+            ctx.fail(403);
     }
 
-    private boolean doIsPermitted( CustomWebUser user, PermisstionAndRoleSet permissionOrRole ){
+
+
+    private boolean doIsPermitted(CustomWebUser user, PermisstionAndRoleSet permissionOrRole) {
         Set<String> roles = user.getRoles();
         Set<String> permissions = user.getPermissions();
-        return roles.contains( "admin" ) || permissionOrRole.isExist( roles ) || permissionOrRole.isExist( permissions );
+        return roles.contains("admin") || permissionOrRole.isExist(roles) || permissionOrRole.isExist(permissions);
 
     }
 
